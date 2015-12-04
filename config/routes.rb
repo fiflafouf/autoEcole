2d5d0e6017470c89e6a3bc1fbd102af3ebfa0a9b
audreybarrios <audrey.barrios65@gmail.com>
2015-12-04 13:30:22 +0000

    authentification (devise)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   
+
+  devise_for :admins
+  devise_for :users
   root 'welcome#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
