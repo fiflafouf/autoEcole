00c6182ef9c2a72814f0f56188ac8ece32a3027c
sfoures <f.sebastien09@gmail.com>
2016-01-05 13:18:13 +0000

    Correction user.rb syntaxe

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
          
-  enum :role, [:admin, :teacher, :student]
+  enum role: [:admin, :teacher, :student]
 end
