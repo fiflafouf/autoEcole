b89f737b726e884191ba60b60434c6c6832dfc99
audreybarrios <audrey.barrios65@gmail.com>
2016-01-19 09:55:50 +0000

    gem calendrier

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   resources :cars
   devise_for :users
   root 'welcome#index'
+  get 'planning'=> 'planning#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
